f1fdf66e5608e6f87ba72a159a3b4ddbeda20705
majharul-islam181 <majharul.flutter@gmail.com>
2025-09-21 11:58:47 +0600

    some import file updated.

--- lib/features/auth/controllers/auth_controller.dart
+++ lib/features/auth/controllers/auth_controller.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:reddit/features/auth/repository/auth_repository.dart';
+import '../repository/auth_repository.dart';
 
 final authControllerProvider = Provider(
     (ref) => AuthController(authRepository: ref.read(authRepositoryProvider)));
